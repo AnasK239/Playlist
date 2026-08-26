df6b09135d7b8511ce64594c296849e2de86b938
Anas Khaled <anaskhaled23.9.4@gmail.com>
2026-08-26 20:44:47 +0300

    intitalize

---
+++ src/test/java/com/pm/playlistapp/PlaylistAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.playlistapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaylistAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
